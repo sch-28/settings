271d6938cf0848150868b30ae92a4ba07d6c3fbe
sch-28 <jansch2803@googlemail.com>
2023-10-25 23:33:39 +0200

    feat: added supertab & changed copilot accept key

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,7 @@
 -- keybdind to dismiss github copilot suggestion
 vim.api.nvim_set_keymap('i', '<C-r>', '<Plug>(copilot-dismiss)', { noremap = true, silent = true })
+-- map accect to  C-j
+vim.cmd([[
+        imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+        let g:copilot_no_tab_map = v:true
+]])
